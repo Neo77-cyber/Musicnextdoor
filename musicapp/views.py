62d1a49d8ab8ecf4790a50344f5bac6fdf474fdd
Neo <neo@Neos-MacBook-Pro.local>
2023-05-07 21:48:29 +0100

    Updated static files

--- musicapp/views.py
+++ musicapp/views.py
@@ -4,9 +4,6 @@ from .forms import BlogForm
 
 # Create your views here.
 
-
-
-
 def home(request):
     posts = MusicBlog.objects.all().order_by('-date')
     
@@ -20,28 +17,19 @@ def post(request, pk):
         form = BlogForm(request.POST)
     if form.is_valid():
             form.save()
-            return redirect('votes') 
-
-
-
-    
-                
+            return redirect('votes')                 
     return render (request, 'post.html', {'post': post, 'form': form})
 
 
 def votes(request):
-    total_votes = Vote.objects.count()
-    
+    total_votes = Vote.objects.count() 
     vote1_count = Vote.objects.filter(vote='this is so good').count()
-    vote1_percent = (vote1_count / total_votes) * 100
-    
-    
+    vote1_percent = (vote1_count / total_votes) * 100  
     vote2_count = Vote.objects.filter(vote='good but no replay value').count()
-    vote2_percent = (vote2_count / total_votes) * 100
-    
+    vote2_percent = (vote2_count / total_votes) * 100  
     vote3_count = Vote.objects.filter(vote='i dont like this').count()
     vote3_percent = (vote3_count / total_votes) * 100
-    
+  
     context = {
         'total_votes': total_votes,
         'vote1_count': vote1_count,
